cfbc6c664341379673ac80b7baefb9bb9a607065
artem-litvinov <esp.raccoon@gmail.com>
2024-06-16 01:08:10 +0200

    Replace slime with villager (+animations)

--- heroes/hero.gd
+++ heroes/hero.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const speed = 300  # Movement speed
 const random_destination_chance = 0.5  # 50% chance to move to a random point
 const threshold_distance = 100 # Threshold distance to consider the target reached
+const sprite_ids = ["Villager_1", "Villager_2", "Villager_3"]
 
 var buildings: Array[Node] = []
 
@@ -10,28 +11,23 @@ var buildings: Array[Node] = []
 
 
 func _ready() -> void:
+	var sprite_id = sprite_ids.pick_random()
+	%HeroCosmetics.setup_cosmetics(sprite_id)
 	# Dynamically find all buildings in the "buildings" group
 	buildings = get_tree().get_nodes_in_group("buildings")
-	%Slime.play_walk()
+	
+	call_deferred("setup_seeker")
 
-	call_deferred("setup_hero")
 
-func setup_hero():
+func setup_seeker():
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
 	set_new_target()
 
 
-func _physics_process(_delta: float) -> void:
-	if navigation_agent.is_navigation_finished():
-		set_new_target()
-		return
-	move_towards_target()
-
-
 func set_new_target() -> void:
 	var new_target: Vector2
-	if randf() < random_destination_chance:
+	if randf() < random_destination_chance || buildings.size() == 0:
 		# Move to a random point on the map
 		new_target = get_random_point()
 	else:
@@ -46,10 +42,26 @@ func get_random_point() -> Vector2:
 	return Vector2(randf() * screen_size.x, randf() * screen_size.y)
 
 
+func _physics_process(_delta: float) -> void:
+	update_animations()
+
+	if navigation_agent.is_navigation_finished():
+		set_new_target()
+		return
+
+	move_towards_target()
+
+
 func move_towards_target() -> void:
 	var direction = global_position.direction_to(navigation_agent.get_next_path_position())
 	navigation_agent.set_velocity(direction * speed)
 
+func update_animations() -> void:
+	var moving_right = velocity.x > 0
+	if velocity.length() > 0:
+		%HeroCosmetics.play_running(moving_right)
+	else:
+		%HeroCosmetics.play_idle()
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
---
+++ heroes/hero_cosmetics.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+const IDLE_ANIM_NAME = "idle"
+const RUNNING_ANIM_NAME = "running"
+const SPRITE_FILE_EXT = ".png"
+
+var sprites_path: String
+var idle_anim_sprites
+var running_anim_sprites
+
+
+func _ready() -> void:
+	var l = %AnimationPlayer.get_animation_list()
+	print_debug(l)
+
+
+func setup_cosmetics(sprite_id: String) -> void:
+	sprites_path = SpriteManager.get_sprite_path(sprite_id)
+	idle_anim_sprites = load(sprites_path + IDLE_ANIM_NAME + SPRITE_FILE_EXT)
+	running_anim_sprites = load(sprites_path + RUNNING_ANIM_NAME + SPRITE_FILE_EXT)
+
+
+func play_idle() -> void:
+	%Sprite2D.texture = idle_anim_sprites
+	%AnimationPlayer.play(IDLE_ANIM_NAME)
+
+
+func play_running(moving_right: bool) -> void:
+	%Sprite2D.flip_h = !moving_right
+	%Sprite2D.texture = running_anim_sprites
+	%AnimationPlayer.play(RUNNING_ANIM_NAME)
