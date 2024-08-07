8e2b356856626d83e09bbc827e34afc08ced6e1f
artem-litvinov <esp.raccoon@gmail.com>
2024-08-07 23:12:37 +0200

    Fix variables shadowing

--- ui/components/event_option.gd
+++ ui/components/event_option.gd
@@ -5,11 +5,11 @@ const MAX_CHAR_PER_LINE = 50
 
 
 # Function to wrap text by inserting '\n' after every MAX_CHAR_PER_LINE characters
-func autowrap_text(text: String) -> String:
+func autowrap_text(_text: String) -> String:
 	var wrapped_text = ""
 	var current_line_length = 0
 
-	var words = text.split(" ")
+	var words = _text.split(" ")
 	for word in words:
 		# If adding the next word would exceed the limit, insert a newline
 		if current_line_length + word.length() > MAX_CHAR_PER_LINE:
@@ -25,6 +25,6 @@ func autowrap_text(text: String) -> String:
 
 
 # Function to set the text of a Label with auto-wrapping
-func set_text_autowrap(text: String) -> void:
-	var wrapped_text = autowrap_text(text)
+func set_text_autowrap(_text: String) -> void:
+	var wrapped_text = autowrap_text(_text)
 	self.text = wrapped_text
