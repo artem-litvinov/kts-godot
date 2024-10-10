52ae6f06927ab3e486be1e7274687d2e085585a7
artem-litvinov <esp.raccoon@gmail.com>
2024-10-10 22:29:42 +0200

    Fix mocks

--- utils/mocks.gd
+++ utils/mocks.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var mock_user: User = User.from_params("12345abcde", "Bob")
-var mock_world_state: WorldState = WorldState.from_params(20, 50, 100, false)
+var mock_world_state: WorldState = WorldState.from_params(100, false)
 var mock_heroes: Array[Hero] = [
 	Hero.from_params(
 		"1",
@@ -47,8 +47,8 @@ var mock_event = Events.AIEvent.from_params(
 	"As the hero trudges through the dense underbrush of the wilds, a glistening, semi-transparent slime catches their eye, undulating gently among the fallen leaves. The creature's vibrant green hue stands out against the earthy tones of the forest floor, its gelatinous form pulsing with an almost hypnotic rhythm. The hero pauses, sensing both the curiosity and the potential danger that this peculiar encounter presents.",
 	Events.EventEnemy.from_params("Slime", 60, 120),
 	[
-		Events.Option.from_params("Attack the slime!", Events.OptionResults.from_params("You killed the slime", -20, 20, 10, 10)),
-		Events.Option.from_params("Lick the slime (•؎ •)", Events.OptionResults.from_params("You licked the slime", -40, 0, -20, 0)),
-		Events.Option.from_params("Run away!", Events.OptionResults.from_params("Your ran off", 0, 0, -10, 0)),
+		Events.Option.from_params("Attack the slime!", Events.OptionResults.from_params("You killed the slime", -20, 20)),
+		Events.Option.from_params("Lick the slime (•؎ •)", Events.OptionResults.from_params("You licked the slime", -40, -20)),
+		Events.Option.from_params("Run away!", Events.OptionResults.from_params("Your ran off", 0, 0)),
 	]
 )
